39232290be83ee692ab2678c096ec29aeae5f2bf
Giorgio <giorgio.pignattari@gmail.it>
2021-05-18 18:18:53 +0200

    stampato ogni movie in una card

--- laravel-model-controller/resources/views/movies.blade.php
+++ laravel-model-controller/resources/views/movies.blade.php
@@ -3,13 +3,18 @@
 @section('main')
 <section class="movies-container container-fluid">
   <h1>Movies</h1>
-  {{ var_dump($movies) }}
   <!-- card section -->
   <div class="card-container container-center">
     <!-- stampo tutte le cards con foreach -->
+    @foreach ($movies as $movie)
       <div class="card pt-50">
-
+        <h2>{{ $movie->title }}</h2>
+        <h2 class="italic">{{ $movie->original_title }}</h2>
+        <h4>{{ $movie->nationality }}</h4>
+        <h4>{{ $movie->date }}</h4>
+        <h1>{{ $movie->vote }}</h1>
       </div>
+    @endforeach  
   </div>
 </section>
 @endsection
